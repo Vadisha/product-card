86e55a106c5c88a2d101ff2f9aa74c8ee3c359de
Vadim <fazvadim@gmail.com>
2025-11-26 18:58:16 +0500

    complete all tasks homework-7 level 1

--- homework-7.js
+++ homework-7.js
@@ -1,3 +1,53 @@
+// 2. Создать массив чисел от 1 до 10. Отфильтровать его таким образом, что бы мы получил массив чисел, начиная с 5.
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
+const filterNumbers = numbers.filter(number => number >= 5)
+console.log(filterNumbers);
 
+// 3. Создать массив строк, относящихся к любой сущности (название фильмов/книг, кухонные приборы, мебель и т.д.), проверить, есть ли в массиве какая-то определенная сущность.
+
+const books = [
+  {
+    title: "Мастер и Маргарита",
+    author: "Михаил Булгаков",
+    year: 1967,
+    coverColor: "черный",
+    genre: "роман"
+  },
+  {
+    title: "1984",
+    author: "Джордж Оруэлл",
+    year: 1949,
+    coverColor: "красный",
+    genre: "антиутопия"
+  },
+    {
+    title: "Преступление и наказание",
+    author: "Федор Достоевский",
+    year: 1866,
+    coverColor: "коричневый",
+    genre: "психологический роман"
+  },
+  {
+    title: "Гарри Поттер и философский камень",
+    author: "Джоан Роулинг",
+    year: 1997,
+    coverColor: "синий",
+    genre: "фэнтези"
+  }
+];
+
+const bookYearFilters = books.filter(book => book.year < 1950);
+const bookAuthorFilters = books.filter(book => book.author.includes('Булгаков'))
+
+console.log(bookYearFilters);
+console.log(bookAuthorFilters)
+
+//4. Написать функцию, которая аргументом будет принимать массив и изменять его порядок на противоположный ("переворачивать") . Два вышеуказанных массива с помощью этой функции перевернуть.
+
+numbers.reverse()
+
+books.reverse()
+
+console.log(numbers)
+console.log(books)
