06524571d847f0be7e2168000c537a84ddc89caa
Vadim <fazvadim@gmail.com>
2025-12-16 11:08:42 +0500

    fixed errors

--- homework-8.js
+++ homework-8.js
@@ -4,49 +4,68 @@ import { products } from "./products.js";
 
 //3. По аналогии из лекции — создать и реализовать шаблон для продуктовых карточек.
 
-const productsTemplate = document.getElementById('products-template');
-const productsList = document.getElementById('products-list');
+const productsTemplate = document.getElementById("card-template");
+const productsList = document.getElementById("products-list");
 
-products.forEach(product => {
-  const productClone = productsTemplate.content.cloneNode(true);
-  productClone.querySelector('.product-image').scr = `/assets/${product.img}.png`
-  productClone.querySelector('.product-category').textContent = product.category
-  productClone.querySelector('.product-name').textContent = product.name
-  productClone.querySelector('.product-description').textContent = product.description
-  productClone.querySelector('.product-composition').textContent = product.composition
-  productClone.querySelector('.product-price').textContent = product.price
-  productsList.appendChild(productClone)
-})
+function fillTemplate(clone, product) { 
+  productClone.querySelector(".product_image").scr = `/images/${product.imgName}.png`
+  productClone.querySelector(".product_image").alt = product.name;
+  productClone.querySelector(".product-category").textContent = product.category
+  productClone.querySelector(".product_name").textContent = product.name
+  productClone.querySelector(".product_description").textContent = product.description
+  
+  const compList = Clone.querySelector(".product_composition");
+  compList.textContent = 'Состав';
+   product.composition.forEach(item => {
+    const li = document.createElement("li");
+    li.textContent = item;
+    productList.appendChild(li);
+  });
+  
+  productClone.querySelector(".product_price-text").textContent = 'Цена';
+  productClone.querySelector(".product_price").textContent = `${product.price}₽`;
+};
 
 //4. Используя метод .reduce(), получить строку, которая состоит из названий продуктовых карточек, разделенных точкой с запятой
 
-const namesString = products.reduce((acc, product, index) => {
-    return index === 0 ? product.name : `${acc};${product.name}`;
-}, '');
-
-console.log(namesString); 
+const getProductNameWithReduce = products.reduce((acc, product) => {
+  acc.push(product.name);
+  return acc;
+}, []);
 
 //5. Используя метод .reduce(), получить массив объектов, где ключем является название продукта, а значением - его описание
 
-const productsDescriptions = products.reduce((acc, product) => {
-    const productObj = {};
-    productObj[product.name] = product.description;
-
-    acc.push(productObj);
-    return acc;
+const getNameAndDescriptionWithReduce = products.reduce((acc, product) => {
+  acc.push({[product.name]: product.description});
+  return acc;
 }, []);
-
-console.log(productsDescriptions);
 
 //6*. Реализовать функцию, которая при старте нашей страницы выводит сообщение с текстом, мол "Сколько карточек отобразить? От 1 до 5" и в зависимости от результата - будет выводить это количество. Должна быть защита от введенных других значений (имеется ввиду проверка if).
 
-const displayCards = () => {
-    const question = prompt("Сколько карточек отобразить? От 1 до 5");
-    const numbers = Number(question)
-    if(numbers > 0 && numbers <= productCards.length) {
-    outputProducts(products.slice(0, numbers))
+function renderCards(count) {
+  productsList.innerHTML = "";
 
-} else {
-    alert("Нужно вводить число от 1 до 5")
-}}
-displayCards();
+  const limitedProducts = products.slice(0, count);
+
+  limitedProducts.forEach(product => {
+    const clone = template.content.cloneNode(true);
+
+    fillTemplate(clone, product);
+
+    productsList.appendChild(clone);
+  });
+}
+
+function askProductCount() {
+  const userInput = prompt("Сколько карточек отобразить? От 1 до 5");
+  const numberUserInput = Number(userInput);
+
+  if (!isNaN(numberUserInput) && numberUserInput >= 1 && numberUserInput <= 5) {
+    return numberUserInput;
+  }
+
+  alert("Введите число от 1 до 5!");
+  return askProductCount();
+}
+
+document.addEventListener("DOMContentLoaded", () => renderCards(askProductCount()));
--- products.js
+++ products.js
@@ -1,6 +1,6 @@
 export const products = [
   {
-    image:"moist-mousse",
+    imgName:"moist-mousse",
     category: "Для нормальной кожи",
     name: "Увлажняющий мусс",
     description: "Глубоко увлажняют кожу лица, оставляя её мягкой и гладкой.",
@@ -12,7 +12,7 @@ export const products = [
     price: 2750
   },
   {
-    image:"moist-mask",
+    imgName:"moist-mask",
     category: "Для нормальной кожи",
     name: "Увлажняющая маска",
     description: "Способствует удерживанию влаги в верхних слоях кожи.",
@@ -24,7 +24,7 @@ export const products = [
     price: 3500
   },
   {
-    image:"face-gel",
+    imgName:"face-gel",
     category: "Для нормальной кожи",
     name: "Гель для умывания",
     description: "Интенсивно очищает, не повреждает защитный барьер кожи.",
@@ -36,7 +36,7 @@ export const products = [
     price: 1650
   },
   {
-    image:"gift-set_1",
+    imgName:"gift-set_1",
     category: "Для нормальной кожи",
     name: "Подарочный набор №1",
     description: "Набор, состоящий из увлажняющего крема и маски.",
@@ -48,7 +48,7 @@ export const products = [
     price: 4750
   },
   {
-    image:"gift-set_5",
+    imgName:"gift-set_5",
     category: "Для нормальной кожи",
     name: "Подарочный набор №5",
     description: "Весь набор средств Invisible symphony, крем, маска, мусс и гель для умывания.",
